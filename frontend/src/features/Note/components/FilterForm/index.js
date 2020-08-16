461f60e30ef5ad6061d53e52aaee64f57b7dd280
Phuc Hoang <hvphuc081299@gmail.com>
2020-08-16 18:18:12 +0700

    add FilterForm

--- frontend/src/features/Note/components/FilterForm/index.js
+++ frontend/src/features/Note/components/FilterForm/index.js
@@ -4,33 +4,47 @@ import "./FilterForm.scss";
 
 function FilterForm(props) {
   const { title, switchTitle, content, switchContent } = props;
-  const filterItem = ({ name, value, setter }) => {
+
+  const data = [
+    {
+      name: "Title",
+      value: title,
+      setter: switchTitle,
+    },
+    {
+      name: "Content",
+      value: content,
+      setter: switchContent,
+    },
+  ];
+
+  if (data[0].value === false) data[1].disabled = true;
+  if (data[1].value === false) data[0].disabled = true;
+
+  console.log(data);
+
+  const filterItem = ({ name, value, setter, disabled = false }) => {
     return (
       <Row key={name}>
         <Col span={8} className="filter-title">
           <span>{name}</span>
         </Col>
-        <Col span={16}>
-          <Switch onChange={setter} defaultChecked={value} />
-        </Col>
+        {disabled ? (
+          <Col span={16}>
+            <Switch onChange={setter} checked={value} disabled />
+          </Col>
+        ) : (
+          <Col span={16}>
+            <Switch onChange={setter} checked={value} />
+          </Col>
+        )}
       </Row>
     );
   };
   // console.log(props);
   return (
     <>
-      {[
-        {
-          name: "Title",
-          value: title,
-          setter: switchTitle,
-        },
-        {
-          name: "Content",
-          value: content,
-          setter: switchContent,
-        },
-      ].map((item) => filterItem(item))}
+      {data.map((item) => filterItem(item))}
     </>
   );
 }
