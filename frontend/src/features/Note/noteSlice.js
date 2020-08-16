c1d072eefcd0920bb18e3dcd5db6ade22ede22bb
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2020-08-16 17:21:34 +0700

    add fetchNote for noteViewer

--- frontend/src/features/Note/noteSlice.js
+++ frontend/src/features/Note/noteSlice.js
@@ -1,34 +1,58 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import noteAPI from '../../api/noteAPI';
 
-const getNote = (id) => {
-  return {
-    title: 'Title',
-    content: '# abc',
-    isFavorite: true,
-    lastEdited: Date.now(),
-    tags: ['tag', 'tag2'],
-    children: ['id1', 'id2']
-  }
-}
+const initialState = {
+  visibleSearchForm: false,
+  favoriteNotes: [],
+  rootNotes: [],
+  notesInView: [{
+    loading: false,
+    data: {},
+    error: {}
+  }, {
+    loading: false,
+    data: {},
+    error: {}
+  }],
+  activeNoteIndex: 0
+};
 
 const note = createSlice({
   name: "note",
-  initialState: {
-    visibleSearchForm: false,
-    left: null,
-    right: null,
-    
-  },
+  initialState,
   reducers: {
     setVisibleSearchForm: (state, action) => {
       state.visibleSearchForm = action.payload;
     },
-    setNote: (state, { id, side }) => {
-      state[side] = getNote(id);
+    setActiveNoteIndex: (state, action) => {
+      state.activeNoteIndex = action.payload;
     },
+    setNote: (state, { field, newValue }) => {
+      state.notesInView[state.activeNoteIndex][field] = newValue;
+    }
+  },
+  extraReducers: {
+    [fetchNote.pending.type]: (state, action) => {
+      state.notesInView[state.activeNoteIndex].loading = true;
+    },
+    [fetchNote.rejected.type]: (state, action) => {
+      console.log('rejected');
+      state.notesInView[state.activeNoteIndex].loading = false;
+      state.notesInView[state.activeNoteIndex].error = action.payload;
+    },
+    [fetchNote.fulfilled.type]: (state, action) => {
+      state.notesInView[state.activeNoteIndex].loading = false;
+      state.notesInView[state.activeNoteIndex].data = action.payload;
+    }
   }
 });
 
+export const fetchNote = createAsyncThunk('note/noteLoading', (id) =>
+  noteAPI.getNote(id)
+    .then(response => response.data)
+    .catch(error => error)
+);
+
 const { reducer, actions } = note;
-export const { setVisibleSearchForm, setNote } = actions;
+export const { setVisibleSearchForm, setNote, setActiveNoteIndex } = actions;
 export default reducer;
