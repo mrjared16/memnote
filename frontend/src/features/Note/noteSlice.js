4d37d92b0d3cb57fdab1e6c2c0410835c905e01c
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2020-08-16 15:35:59 +0700

    modify to test NoteViewer component

--- frontend/src/features/Note/noteSlice.js
+++ frontend/src/features/Note/noteSlice.js
@@ -1,17 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const getNote = (id) => {
+  return {
+    title: 'Title',
+    content: '# abc',
+    isFavorite: true,
+    lastEdited: Date.now(),
+    tags: ['tag', 'tag2'],
+    children: ['id1', 'id2']
+  }
+}
+
 const note = createSlice({
   name: "note",
   initialState: {
-    visibleSearchForm: false
+    visibleSearchForm: false,
+    left: null,
+    right: null,
+    
   },
   reducers: {
     setVisibleSearchForm: (state, action) => {
       state.visibleSearchForm = action.payload;
-    }
+    },
+    setNote: (state, { id, side }) => {
+      state[side] = getNote(id);
+    },
   }
 });
 
 const { reducer, actions } = note;
-export const { setVisibleSearchForm } = actions;
+export const { setVisibleSearchForm, setNote } = actions;
 export default reducer;
