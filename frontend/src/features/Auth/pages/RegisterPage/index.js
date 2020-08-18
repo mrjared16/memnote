7ea1542ff0e8e54503f3919f1e0428acb9a9804d
Phuc Hoang <hvphuc081299@gmail.com>
2020-08-19 03:09:10 +0700

    form register

---
+++ frontend/src/features/Auth/pages/RegisterPage/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./RegisterPage.scss";
+import { Row, Col } from "antd";
+import RegisterForm from "../../components/RegisterForm";
+import { setToken } from "../../authSlice";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import authApi from "../../../../api/authApi";
+
+function RegisterPage(props) {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleRegister = async (values) => {
+    const { username, password, email } = values;
+    const params = {
+      username,
+      password,
+      email
+    }
+    authApi.register(params).then(res => {
+      dispatch(setToken(res));
+      history.push('/');
+    })
+  };
+
+  return (
+    <Row className="login-row">
+      <Col span={8}></Col>
+      <Col span={8} className="login-col">
+        <RegisterForm onSubmit={handleRegister} />
+      </Col>
+      <Col span={8}></Col>
+    </Row>
+  );
+}
+
+export default RegisterPage;
