56dca949e662c9a98153f1990c9167b02e3cca52
Phuc Hoang <hvphuc081299@gmail.com>
2020-08-18 16:11:30 +0700

    add backend folder and set up axios

---
+++ backend/api/controllers/user.js
@@ -0,0 +1,27 @@
+exports.login = (req, res, next) => {
+
+}
+
+exports.logout = (req, res, next) => {
+
+}
+
+exports.register = (req, res, next) => {
+
+}
+
+exports.forgotPassword = (req, res, next) => {
+
+}
+
+exports.getProfile = (req, res, next) => {
+
+}
+
+exports.updateProfile = (req, res, next) => {
+
+}
+
+exports.changePassword = (req, res, next) => {
+
+}
---
+++ backend/api/routes/tags.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.patch('/:id', function(req, res, next) {
+});
+
+router.delete('/:id', function(req, res, next) {
+});
+
+module.exports = router;
