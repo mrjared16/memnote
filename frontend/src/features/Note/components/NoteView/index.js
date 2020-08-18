5ce5d88ebf172e5d4c9d31c6ed0f4b9cde18fbc6
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2020-08-18 23:16:59 +0700

    add loading state and note not found to note view

--- frontend/src/features/Note/components/NoteView/index.js
+++ frontend/src/features/Note/components/NoteView/index.js
@@ -6,12 +6,13 @@ import MyButton from '../Button';
 import ReactMarkdown from 'react-markdown';
 import ContentEditable from "react-contenteditable";
 import dateFormat from 'dateformat';
+import Loading from '../Loading';
 
 const { Text, Title } = Typography;
 const { TextArea } = Input;
 
 function NoteView(props) {
-    const { note, splitMode, handleChange, handleEvent } = props;
+    const { note = {}, splitMode, handleChange, handleEvent, isLoading } = props;
 
     const {
         id,
@@ -22,6 +23,7 @@ function NoteView(props) {
         tags = [],
         children
     } = note;
+    
     console.log(note);
     const noteID = id;
 
@@ -53,73 +55,88 @@ function NoteView(props) {
 
     }
     return (
-        <div className='viewer'>
-            <div className='viewer-header'>
-                <Row>
-                    <Col flex='auto' className='viewer-tool-left'>
-                        <Space>
-                            <MyButton className={isFavorite && 'active'}
-                                label='Favorite'
-                                onClick={handleToggleFavorite}
-                                popover={true}
-                                icon={isFavorite ?
-                                    <HeartFilled /> :
-                                    <HeartOutlined />} />
-                            <MyButton label='Delete' onClick={handleDeleteNote} popover={true} icon={<DeleteOutlined />} />
-                            <Text className='text'>Last edited: {getFormatedDate()} </Text>
-                        </Space>
-                    </Col>
-                    <Col flex='auto' className='viewer-tool-right'>
-                        <Space >
-                            <MyButton label='Save' className='save-btn' onClick={handleSaveNote} />
-                            {
-                                previewMode ?
-                                    <MyButton label={'Edit'} onClick={handleTogglePreviewMode} popover={true} icon={<EditOutlined />} /> :
-                                    <MyButton label={'Preview'} onClick={handleTogglePreviewMode} popover={true} icon={<EyeOutlined />} />
-                            }
+        <>
+            {isLoading ?
+                <Loading className='viewer-main' /> :
+                (<>
+                    {id
+                        ?
+                        <div className='viewer'>
+                            <div className='viewer-header'>
+                                <Row>
+                                    <Col flex='auto' className='viewer-tool-left'>
+                                        <Space>
+                                            <MyButton className={isFavorite && 'active'}
+                                                label='Favorite'
+                                                onClick={handleToggleFavorite}
+                                                popover={true}
+                                                icon={isFavorite ?
+                                                    <HeartFilled /> :
+                                                    <HeartOutlined />} />
+                                            <MyButton label='Delete' onClick={handleDeleteNote} popover={true} icon={<DeleteOutlined />} />
+                                            <Text className='text'>Last edited: {getFormatedDate()} </Text>
+                                        </Space>
+                                    </Col>
+                                    <Col flex='auto' className='viewer-tool-right'>
+                                        <Space >
+                                            <MyButton label='Save' className='save-btn' onClick={handleSaveNote} />
+                                            {
+                                                previewMode ?
+                                                    <MyButton label={'Edit'} onClick={handleTogglePreviewMode} popover={true} icon={<EditOutlined />} /> :
+                                                    <MyButton label={'Preview'} onClick={handleTogglePreviewMode} popover={true} icon={<EyeOutlined />} />
+                                            }
 
-                            {splitMode ?
-                                <MyButton label='Close' onClick={handleCloseSplitPane} popover={true} icon={<CloseOutlined />} /> :
-                                <MyButton label='Split' onClick={handleOpenSplitPane} popover={true} icon={<SplitCellsOutlined />} />
-                            }
-                        </Space>
-                    </Col>
-                </Row>
-                <Row className='tags'>
-                    <Space>
-                        <MyButton label='Tags' className='add-tag-btn' onClick={handleAddTag} icon={<TagsOutlined />} />
-                        <Space className='tag-list'>
-                            {
-                                tags.map(({ id, name }) => {
-                                    return (
-                                        <Tag className='tag' key={id} closable onClose={handleRemoveTagFromNote(id, noteID)}>{name}</Tag>
-                                    )
-                                })
-                            }
-                        </Space>
-                    </Space>
-                </Row>
-            </div>
+                                            {splitMode ?
+                                                <MyButton label='Close' onClick={handleCloseSplitPane} popover={true} icon={<CloseOutlined />} /> :
+                                                <MyButton label='Split' onClick={handleOpenSplitPane} popover={true} icon={<SplitCellsOutlined />} />
+                                            }
+                                        </Space>
+                                    </Col>
+                                </Row>
+                                <Row className='tags'>
+                                    <Space>
+                                        <MyButton label='Tags' className='add-tag-btn' onClick={handleAddTag} icon={<TagsOutlined />} />
+                                        <Space className='tag-list'>
+                                            {
+                                                tags.map(({ id, name }) => {
+                                                    return (
+                                                        <Tag className='tag' key={id} closable onClose={handleRemoveTagFromNote(id, noteID)}>{name}</Tag>
+                                                    )
+                                                })
+                                            }
+                                        </Space>
+                                    </Space>
+                                </Row>
+                            </div>
 
-            <Row className='viewer-main'>
-                <Col flex='auto'></Col>
-                <Col flex={splitMode ? '6' : '3'}>
+                            <Row className='viewer-main'>
+                                <Col flex='auto'></Col>
+                                <Col flex={splitMode ? '6' : '3'}>
 
-                    <h1 className='title-content ant-typography'>
-                        <ContentEditable
-                            html={`${title}`}
-                            onChange={onTitleChange}
-                        />
-                    </h1>
+                                    <h1 className='title-content ant-typography'>
+                                        <ContentEditable
+                                            html={`${title}`}
+                                            onChange={onTitleChange}
+                                        />
+                                    </h1>
 
-                    {previewMode ?
-                        <ReactMarkdown className='note-content reset' source={content} /> :
-                        <TextArea className='note-content' autoSize={true} value={content} bordered={false} onChange={handleContentChange} />
+                                    {previewMode ?
+                                        <ReactMarkdown className='note-content reset' source={content} /> :
+                                        <TextArea className='note-content' autoSize={true} value={content} bordered={false} onChange={handleContentChange} />
+                                    }
+                                </Col>
+                                <Col flex='auto'></Col>
+                            </Row>
+                        </div >
+                        :
+                        <>
+                            <Text>Note not found</Text>
+                        </>
                     }
-                </Col>
-                <Col flex='auto'></Col>
-            </Row>
-        </div >
+                </>
+                )
+            }
+        </>
     );
 }
 
