4f00ca914abfb2e3b8ad3108878b31cdda613368
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2020-08-19 10:16:28 +0700

    add ID field to tag component

--- frontend/src/features/Note/components/ListTag/index.js
+++ frontend/src/features/Note/components/ListTag/index.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import Tag from "../Tag";
-import { Input } from "antd";
+import { Input, Space } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import "./ListTag.scss";
 
 function ListTag(props) {
-  const { listTags, onAdd, onRemove } = props;
+  const { tags, onAdd, onRemove } = props;
 
   const [inputVisible, setInputVisible] = useState(false);
   const [inputValue, setInputValue] = useState("");
@@ -19,33 +19,38 @@ function ListTag(props) {
   };
 
   const handleInputConfirm = () => {
-    onAdd({ tag: inputValue });
+    onAdd({ tagName: inputValue });
     setInputValue("");
     setInputVisible(false);
   };
   // sort => sort : not render
   return (
-    <div className="list-tags">
-      {listTags.map((name) => (
-        <Tag title={name} onClose={onRemove} />
-      ))}
+    <div className="list-tag">
+      {
+        tags.map(({ name = '', id = '' }, index) => (
+          <Tag className='tag' key={id ? id : index} title={name} onClose={onRemove} />
+        ))
+      }
+
       {inputVisible && (
         <Input
           autoFocus
           type="text"
           size="small"
-          className="tag-input"
+          className="add-tag-input tag"
           value={inputValue}
           onChange={handleInputChange}
           onBlur={handleInputConfirm}
           onPressEnter={handleInputConfirm}
         />
       )}
+
       {!inputVisible && (
         <Tag
+          className='add-tag-btn tag'
           closable={false}
           icon={<PlusOutlined />}
-          title="New Tag"
+          title="Add Tag"
           onClick={showInput}
         />
       )}
